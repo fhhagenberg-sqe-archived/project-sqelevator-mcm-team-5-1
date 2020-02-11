cbd90081d7cf7e2e417e66997dfa9c04f1fbc761
Linus Heimböck <linus.heimboeck@live.at>
2020-02-11 21:41:43 +0100

    Some javadoc

--- src/main/java/at/fhhagenberg/sqelevator/interfaces/IEccObservable.java
+++ src/main/java/at/fhhagenberg/sqelevator/interfaces/IEccObservable.java
@@ -2,11 +2,26 @@ package at.fhhagenberg.sqelevator.interfaces;
 
 import at.fhhagenberg.sqelevator.model.ApplicationState;
 
+/**
+ * Observable interface. Allows a class to be observed by others and to notify them when the status changes.
+ */
 public interface IEccObservable {
 
+    /**
+     * Adds an observer to the observed class.
+     * @param observer The observer to be added.
+     */
     void addObserver(IEccObserver observer);
 
+    /**
+     * Removes the specified observer from the list of observers of a class.
+     * @param observer The observer to remove.
+     */
     void removeObserver(IEccObserver observer);
 
+    /**
+     * Notifies all registered observers of changes to the application state.
+     * @param applicationState The new, updated state of the application.
+     */
     void notifyObservers(ApplicationState applicationState);
 }
