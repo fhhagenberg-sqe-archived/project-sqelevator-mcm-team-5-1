3685e9caa94952a46e2e408eb0638c7c1b673b1c
Linus Heimböck <linus.heimboeck@live.at>
2020-02-11 13:49:25 +0100

    Very shitty autopilot

--- src/main/java/at/fhhagenberg/sqelevator/model/ApplicationModel.java
+++ src/main/java/at/fhhagenberg/sqelevator/model/ApplicationModel.java
@@ -48,10 +48,192 @@ public class ApplicationModel extends EccModel {
             }
             applicationState.elevators = elevatorList;
 
+            autoOperateElevator(0);
             notifyObservers(applicationState);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void autoOperateElevator(int elevatorIndex) throws RemoteException {
+
+        Elevator elevator = applicationState.elevators.get(elevatorIndex);
+
+        switch (elevator.committedDirection) {
+            case IElevator.ELEVATOR_DIRECTION_UP: {
+
+                if (elevator.currentFloor == elevator.currentTarget && elevator.currentSpeed == 0 && elevator.doorStatus == IElevator.ELEVATOR_DOORS_OPEN) {
+                    int target = getNextFloorTargetUpwards(elevator);
+
+                    if (elevator.currentTarget != target) {
+                        elevatorControl.setTarget(elevatorIndex, target);
+                    } else {
+                        elevatorControl.setCommittedDirection(elevatorIndex, IElevator.ELEVATOR_DIRECTION_UNCOMMITTED);
+                    }
+                }
+                break;
+            }
+            case IElevator.ELEVATOR_DIRECTION_DOWN: {
+
+                if (elevator.currentFloor == elevator.currentTarget && elevator.currentSpeed == 0 && elevator.doorStatus == IElevator.ELEVATOR_DOORS_OPEN) {
+                    int target = getNextFloorTargetDownwards(elevator);
+
+                    if (elevator.currentTarget != target) {
+                        elevatorControl.setTarget(elevatorIndex, target);
+                    } else {
+                        elevatorControl.setCommittedDirection(elevatorIndex, IElevator.ELEVATOR_DIRECTION_UNCOMMITTED);
+                    }
+                }
+                break;
+            }
+            case IElevator.ELEVATOR_DIRECTION_UNCOMMITTED: {
+                if (elevator.activeFloorButtons.size() > 0) {
+
+                    int goingUp = getAmountHigherThan(elevator.activeFloorButtons, elevator.currentFloor);
+                    int goingDown = getAmountLowerThan(elevator.activeFloorButtons, elevator.currentFloor);
+
+                    if (goingUp >= goingDown) {
+                        elevatorControl.setCommittedDirection(elevatorIndex, IElevator.ELEVATOR_DIRECTION_UP);
+                    } else {
+                        elevatorControl.setCommittedDirection(elevatorIndex, IElevator.ELEVATOR_DIRECTION_DOWN);
+                    }
+                } else if (applicationState.buttonDownPressed.size() > 0 || applicationState.buttonUpPressed.size() > 0) {
+
+                    int goingUp = getAmountHigherThan(applicationState.buttonUpPressed, elevator.currentFloor) +
+                            getAmountHigherThan(applicationState.buttonDownPressed, elevator.currentFloor);
+                    int goingDown = getAmountLowerThan(applicationState.buttonDownPressed, elevator.currentFloor) +
+                            getAmountLowerThan(applicationState.buttonUpPressed, elevator.currentFloor);
+
+                    if (goingUp >= goingDown) {
+                        elevatorControl.setCommittedDirection(elevatorIndex, IElevator.ELEVATOR_DIRECTION_UP);
+                    } else {
+                        elevatorControl.setCommittedDirection(elevatorIndex, IElevator.ELEVATOR_DIRECTION_DOWN);
+                    }
+                }
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+    }
+
+    public int getNextFloorTargetDownwards(Elevator elevator) {
+        int activeFloorButtonIndex = getNextIndexLowerThan(elevator.activeFloorButtons, elevator.currentFloor);
+        int buttonDownIndex = getNextIndexLowerThan(applicationState.buttonDownPressed, elevator.currentFloor);
+
+        int target;
+        if (activeFloorButtonIndex < 0 && buttonDownIndex < 0) {
+
+            // Check if someone is lower down who wants to go up
+            int buttonUpIndex = getNextIndexHigherThan(applicationState.buttonUpPressed, elevator.currentFloor);
+            if (buttonUpIndex < 0) {
+                // Set direction to uncommitted, no need to go higher
+                target = elevator.currentFloor;
+            } else {
+                target = applicationState.buttonUpPressed.get(buttonUpIndex);
+            }
+        } else if (activeFloorButtonIndex < 0) {
+            return applicationState.buttonDownPressed.get(buttonDownIndex);
+        } else if (buttonDownIndex < 0) {
+            return elevator.activeFloorButtons.get(activeFloorButtonIndex);
+        }
+        else {
+            int activeFloorButtonValue = elevator.activeFloorButtons.get(activeFloorButtonIndex);
+            int buttonDownValue = applicationState.buttonDownPressed.get(buttonDownIndex);
+
+            target = activeFloorButtonValue <= buttonDownValue ? activeFloorButtonValue : buttonDownValue;
+        }
+        return target;
+    }
+
+    public int getNextFloorTargetUpwards(Elevator elevator) {
+        int activeFloorButtonIndex = getNextIndexHigherThan(elevator.activeFloorButtons, elevator.currentFloor);
+        int buttonUpIndex = getNextIndexHigherThan(applicationState.buttonUpPressed, elevator.currentFloor);
+
+        int target;
+        if (activeFloorButtonIndex < 0 && buttonUpIndex < 0) {
+
+            // Check if someone is higher up who wants to go down
+            int buttonDownIndex = getNextIndexHigherThan(applicationState.buttonDownPressed, elevator.currentFloor);
+            if (buttonDownIndex < 0) {
+                // Set direction to uncommitted, no need to go higher
+                target = elevator.currentFloor;
+            } else {
+                target = applicationState.buttonDownPressed.get(buttonDownIndex);
+            }
+        } else if (activeFloorButtonIndex < 0) {
+            return applicationState.buttonUpPressed.get(buttonUpIndex);
+        } else if (buttonUpIndex < 0) {
+            return elevator.activeFloorButtons.get(activeFloorButtonIndex);
+        } else {
+            int activeFloorButtonValue = elevator.activeFloorButtons.get(activeFloorButtonIndex);
+            int buttonUpValue = applicationState.buttonUpPressed.get(buttonUpIndex);
+
+            target = activeFloorButtonValue <= buttonUpValue ? activeFloorButtonValue : buttonUpValue;
+        }
+        return target;
+    }
+
+    /** @noinspection Duplicates*/
+    public int getAmountLowerThan(ArrayList<Integer> list, int value) {
+        int lowerCount = 0;
+        for (Integer number : list) {
+            if (number < value) {
+                lowerCount++;
+            }
+        }
+        return lowerCount;
+    }
+
+    /** @noinspection Duplicates*/
+    public int getAmountHigherThan(ArrayList<Integer> list, int value) {
+
+        int higherCount = 0;
+        for (Integer number : list) {
+            if (number > value) {
+                higherCount++;
+            }
+        }
+        return higherCount;
+    }
+
+    public int getNextIndexHigherThan(ArrayList<Integer> list, int value) {
+
+        int index = -1;
+        int difference = -1;
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) > value) {
+
+                int currentDifference = list.get(i) - value;
+                if (difference < 0 || currentDifference < difference) {
+                    difference = currentDifference;
+                    index = i;
+                }
+            }
+        }
+
+        return index;
+    }
+
+    public int getNextIndexLowerThan(ArrayList<Integer> list, int value) {
+
+        int index = -1;
+        int difference = -1;
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) < value) {
+
+                int currentDifference = value - list.get(i);
+                if (difference < 0 || currentDifference < difference) {
+                    difference = currentDifference;
+                    index = i;
+                }
+            }
+        }
+
+        return index;
     }
 
     public void updateUpDownRequestLists(int numberOfFloors) throws RemoteException {
@@ -81,6 +263,7 @@ public class ApplicationModel extends EccModel {
 
         Elevator result = new Elevator();
         result.committedDirection = elevatorControl.getCommittedDirection(elevatorIndex);
+        result.currentTarget = elevatorControl.getTarget(elevatorIndex);
         result.currentAcceleration = elevatorControl.getElevatorAccel(elevatorIndex);
         result.doorStatus = elevatorControl.getElevatorDoorStatus(elevatorIndex);
         result.currentFloor = elevatorControl.getElevatorFloor(elevatorIndex);
--- src/main/java/at/fhhagenberg/sqelevator/model/Elevator.java
+++ src/main/java/at/fhhagenberg/sqelevator/model/Elevator.java
@@ -6,6 +6,8 @@ public class Elevator {
 
     public int committedDirection;
 
+    public int currentTarget;
+
     public int currentAcceleration;
 
     public int doorStatus;
@@ -25,13 +27,20 @@ public class Elevator {
     public String toString() {
         StringBuilder builder = new StringBuilder();
         builder.append("Committed Direction: ").append(committedDirection).append("\n");
+        builder.append("Current Target: ").append(currentTarget).append("\n");
         builder.append("Current Acceleration: ").append(currentAcceleration).append("\n");
         builder.append("Door Status: ").append(doorStatus).append("\n");
         builder.append("Current Floor: ").append(currentFloor).append("\n");
         builder.append("Current Height: ").append(currentHeightOverGround).append("\n");
         builder.append("Current Speed: ").append(currentSpeed).append("\n");
         builder.append("Current Weight: ").append(currentPassengerWeight).append("\n");
-        builder.append("Max Passengers: ").append(maxPassengerNumber);
+        builder.append("Max Passengers: ").append(maxPassengerNumber).append("\n");
+        builder.append("Elevator Buttons Pressed: ");
+
+        for (Integer button : activeFloorButtons) {
+            builder.append(button).append(", ");
+        }
+
         return builder.toString();
     }
 }
