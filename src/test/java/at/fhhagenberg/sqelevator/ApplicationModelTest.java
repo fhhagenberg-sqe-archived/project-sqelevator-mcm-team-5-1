6fc097fa37ddd989d69f9ef7cf8af98d8ee7c0a4
Linus Heimböck <linus.heimboeck@live.at>
2020-02-12 09:34:05 +0100

    Junit example

---
+++ src/test/java/at/fhhagenberg/sqelevator/ApplicationModelTest.java
@@ -0,0 +1,14 @@
+package at.fhhagenberg.sqelevator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationModelTest {
+
+    @Test
+    public void testSetElevatorAutomaticMode() {
+
+        Assertions.assertEquals(1, 1);
+    }
+}
